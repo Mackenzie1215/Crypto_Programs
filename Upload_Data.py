8d9d7945c5ab3bec67e1334036d6344a66cabde9
Mackenzie1215 <64882508+Mackenzie1215@users.noreply.github.com>
2021-05-11 02:44:49 -0400

    Update Upload_Data.py

--- Upload_Data.py
+++ Upload_Data.py
@@ -9,7 +9,7 @@ subprocess.call(['git','init'])
 subprocess.call(['git', 'add', 'CryptoLocalData\LTC_File_LowDetail.csv'])
 subprocess.call(['git', 'commit', '-m', '{}'.format(commit_message)])
 subprocess.call(['git','branch', '-M', 'main'])
-subprocess.call(['git', 'push','-f','--all', 'https://Mackenzie1215:Mack12231998@github.com/Mackenzie1215/Crypto'.format('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')])
+subprocess.call(['git', 'push','-f','--all', 'https://Your Repository address)])
 
 
 commit_message = 'Automtically Committed'
@@ -17,7 +17,7 @@ subprocess.call(['git','init'])
 subprocess.call(['git', 'add', 'CryptoLocalData\LTC_File.csv'])
 subprocess.call(['git', 'commit', '-m', '{}'.format(commit_message)])
 subprocess.call(['git','branch', '-M', 'main'])
-subprocess.call(['git', 'push','-f','--all', 'https://Mackenzie1215:Mack12231998@github.com/Mackenzie1215/Crypto'.format('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')])
+subprocess.call(['git', 'push','-f','--all', 'your repository address')])
 
 time.sleep(5)
 os.system('python wait.py')
